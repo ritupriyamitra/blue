9ba76c356d4d80b915bf2f51b340436823845d1f
HOME <HOME@192.168.0.51>
2019-05-19 21:47:24 -0700

    Corrected imports.

--- src/main/java/com/lazybuds/UserController.java
+++ src/main/java/com/lazybuds/UserController.java
@@ -1,14 +1,11 @@
 package com.lazybuds;
 
-import java.util.Locale;
-
 import javax.transaction.Transactional;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.lazybuds.dao.UserDao;
-import com.lazybuds.exceptions.ValidationException;
-import com.lazybuds.model.ErrorMessage;
 import com.lazybuds.model.User;
 import com.lazybuds.validation.UserValidation;
 
